08ac4351902f55f3603bb8e51fa754db8ec4b576
SeniorKullken <stefan.kull@gmail.com>
2022-03-28 15:48:52 +0200

    ROS2 boilerplate

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'pet_mk_viii'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='seniorKullken',
+    maintainer_email='stefan.kull@gmail.com',
+    description='Main folder for Pet-Mk.IV - The Dashboard',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "pet_ledlights_node=pet_mk_viii.pet_ledlights_node:main"
+        ],
+    },
+)
