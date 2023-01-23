d5d7134880580b7475a3f023c731679bfe965d49
SeniorKullken <stefan.kull@gmail.com>
2023-01-23 17:53:52 +0100

    Added dockerfile + build scripts for Pet-Mk.VIII

---
+++ docker/pet-base-humble/docker_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run docker image
+docker run --interactive \
+ --rm \
+ --network=host \
+ --privileged \
+ kullken/ros2_arm64v8:pet-base-humble
---
+++ docker/pet-mk-viii-runtime/docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Build Dockerfile in the same directory
+docker build --tag kullken/ros2_arm64v8:pet-mk-viii-runtime .
