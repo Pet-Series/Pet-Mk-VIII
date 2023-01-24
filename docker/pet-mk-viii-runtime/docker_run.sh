3e974f24cd6cf128e1121ccb412bf718d0085d3b
SeniorKullken <stefan.kull@gmail.com>
2023-01-24 17:28:22 +0100

    Ver 1.0 of Docker setup for Pet-Mk-VIII

--- docker/pet-mk-viii-runtime/docker_run.sh
+++ docker/pet-mk-viii-runtime/docker_run.sh
@@ -4,4 +4,5 @@ docker run --interactive \
  --rm \
  --network=host \
  --privileged \
+ --volume="/home/pi/ws_ros2:/home/pet/ws_ros2" \
 kullken/ros2_arm64v8:pet-mk-viii-runtime
--- docker/pet-mk-viii-runtime/entrypoint.sh
+++ docker/pet-mk-viii-runtime/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Launch from docker image pet-mk-viii-runtime
+set -e
 source /home/pet/ws_ros2/install/setup.bash
-#exec ros2 launch pet_mk_viii panel_led[0..4]_subscribers.launch.py
-exec ros2 launch pet_mk_viii pet-mk-viii-bringup.py
-#ros2 run pet_mk_viii pet_buttons_node.py &
-#ros2 run pet_mk_viii_joystick pet_potentiometer_node.py &
-#echo 'Done'
+exec "$@"
