ae55e24dd509372afa5d58e4dea9f1241617b406
SeniorKullken <stefan.kull@gmail.com>
2023-01-24 17:29:41 +0100

    Correct file name "*.launch.py"

--- launch/pet-mk-viii-bringup.launch.py
+++ launch/pet-mk-viii-bringup.launch.py
@@ -6,7 +6,7 @@
 # The MIT License (MIT)
 #
 # ROS2 lanchfile
-# Launches 5 nodes. One for each LED[0..4]=5 on the panel.
+# Launches ALL nodes for Pet-Mk.VIII (aka "The Dashboard")
 # 
 # 1) Remember to add '<exec_depend>....</exec_depend>' in package.xml
 # 2) Remember to add '(os.path.join('share', package_name), glob('launch/*.launch.py'))' in setup.py
@@ -14,6 +14,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# GPIO22 = "main_switch"
+# GPIO12 = "joystick_button"
 LED4 =  5 # GPIO05 (Pin 29) for the pet_ledlights_node
 LED3 =  6 # GPIO06 (Pin 31) for the pet_ledlights_node
 LED2 = 13 # GPIO13 (Pin 33) for the pet_ledlights_node
@@ -23,13 +25,24 @@ LED0 = 26 # GPIO26 (Pin 37) for the pet_ledlights_node
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # Launch a common node for the two buttons
+    # Launch a common node for the two buttons on "The Dashboard"
     buttons_node = Node( 
         package="pet_mk_viii",
         executable="pet_buttons_node"
     )
 
-    # Launch one node for each LED[0..4]=5 on the panel.
+    # Launch node for handeling the analog joystick on "The Dashboard"
+    joystick_node = Node(
+        package="pet_ros2_joystick_pkg",
+        executable="pet_joystick_node",
+        parameters=[
+            {"ros_topic_twist":         'twist/cmd_vel'},
+            {'ros_topic_twist_stamped': 'twist_stamped/cmd_vel'},
+            {'ros_topic_raw':           'raw/joystick'},
+        ]
+    )
+
+    # Launch one node for each LED[0..4]=5 on "The Dashboard"
     led0_node = Node( 
         package="pet_mk_viii",
         executable="pet_ledlights_node",
@@ -76,6 +89,7 @@ def generate_launch_description():
     )
 
     # Expose nodes
+    ld.add_action(joystick_node)
     ld.add_action(buttons_node)
     ld.add_action(led0_node)
     ld.add_action(led1_node)
