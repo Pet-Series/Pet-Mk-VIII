25405cd8aba0fa038da25d9dd4b3828a09aa2ab1
SeniorKullken <stefan.kull@gmail.com>
2023-01-29 22:26:11 +0100

    New ENTRYPOINT default CMD "/bin/bash"

--- docker/pet-mk-v-runtime/docker_push.sh
+++ docker/pet-mk-v-runtime/docker_push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Push a Docker Image to DockerHub repository https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
-# https://hub.docker.com/repositories/kullken
 # docker login -u "kullken" -p "@@@@@@@@" docker.io
 docker push kullken/ros2_arm64v8:pet-mk-v-runtime
--- docker/pet-mk-v-runtime/docker_run.sh
+++ docker/pet-mk-v-runtime/docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run docker image from https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
+# Default 'CMD ["ros2", "launch", "pet_mk_viii", "pet-mk-viii-bringup.launch.py"]'
 docker run \
  -it \
  --rm \
