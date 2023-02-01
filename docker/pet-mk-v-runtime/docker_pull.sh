52cc9c71f5a3233ce4ecb8bdd2d207e8af1c92ee
SeniorKullken <stefan.kull@gmail.com>
2023-02-01 18:05:18 +0100

    Docker Pull both base and Pet-Mk.V image

--- docker/pet-mk-v-runtime/docker_pull.sh
+++ docker/pet-mk-v-runtime/docker_pull.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Download docker image from https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
+docker pull kullken/ros2_arm64v8:pet-base-humble
 docker pull kullken/ros2_arm64v8:pet-mk-v-runtime
